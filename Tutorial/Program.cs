aa5b693005cc33790c4e0880d5b5b0bcc5d5a921
Keggii <90061137+Keggii@users.noreply.github.com>
2022-03-23 20:59:22 +0100

    prime numbers

---
+++ Tutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("from where to where do you wanna know the prime numbers?");
+            
+            string[] betterInput = Console.ReadLine().Split(" ");
+            
+            for (int j = Convert.ToInt32(betterInput[0]); j <= Convert.ToInt32(betterInput[1]); j++)
+            {
+                bool primeNumber = true;
+                for (int i = 2; i < j && primeNumber; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        primeNumber = false;
+                    }
+                }
+
+                if (primeNumber)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+        }
+    }
+}
+
