ad6ea36aece653d12e2ec8798ca37bb9daab2d10
Keggii <90061137+Keggii@users.noreply.github.com>
2022-03-24 00:37:12 +0100

    calculators

---
+++ Tutorial/Avarage.cs
@@ -0,0 +1,21 @@
+﻿namespace Tutorial;
+
+public class Avarage
+{
+    public static void AvarageCalc()
+    {
+        Console.WriteLine("from what number do you wanna have the avarage? (split them with space)");
+        String[] numbers = Console.ReadLine().Split(" ");
+        int avarage = 0;
+        int j = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            avarage += Convert.ToInt32(numbers[i]);
+            j++;
+        }
+
+        avarage /= j;
+        Console.WriteLine(avarage);
+    }
+}
--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -1,30 +1,18 @@
-﻿namespace Tutorial
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("from where to where do you wanna know the prime numbers?");
-            
-            string[] betterInput = Console.ReadLine().Split(" ");
-            
-            for (int j = Convert.ToInt32(betterInput[0]); j <= Convert.ToInt32(betterInput[1]); j++)
-            {
-                bool primeNumber = true;
-                for (int i = 2; i < j && primeNumber; i++)
-                {
-                    if (j % i == 0)
-                    {
-                        primeNumber = false;
-                    }
-                }
+﻿namespace Tutorial;
 
-                if (primeNumber)
-                {
-                    Console.WriteLine(j);
-                }
-            }
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1) Prime numbers calculator\n2) Avarage calculator");
+        switch (Console.ReadLine())
+        {
+            case "1":
+                PrimeNumbers.PrimeNumberCalc();
+                break;
+            case "2":
+                Avarage.AvarageCalc();
+                break;
         }
     }
 }
-
