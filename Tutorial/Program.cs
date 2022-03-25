127580f1c1db021e87f2cb0146bcd6519076a32c
Keggii <90061137+Keggii@users.noreply.github.com>
2022-03-25 21:26:42 +0100

    cock

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("1) Prime numbers calculator\n2) Avarage calculator");
+        Console.WriteLine("1) Prime numbers calculator\n2) Avarage calculator\n3) test your luck");
         switch (Console.ReadLine())
         {
             case "1":
@@ -12,6 +12,9 @@ public class Program
                 break;
             case "2":
                 Avarage.AvarageCalc();
+                break;
+            case "3":
+                RandomNumber.Guesser();
                 break;
         }
     }
---
+++ Tutorial/RandomNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace Tutorial;
+
+public class RandomNumber
+{
+    public static void Guesser()
+    {
+        Random rand = new Random();
+        Console.WriteLine("Guesse a number between 1 and 10");
+        byte randomNumber = Convert.ToByte( rand.Next(1, 10) );
+        byte input = Convert.ToByte(Console.ReadLine());
+        byte i = 1;
+        while (randomNumber != input)
+        {
+            input = Convert.ToByte(Console.ReadLine());
+            i++;
+        }
+
+        if (i <= 4)
+            Console.WriteLine("Nice! You only needed" + i + "tries");
+        else
+            Console.WriteLine("Really? You needed " + i + " tries?");
+    }
+}
